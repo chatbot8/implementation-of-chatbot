8522726b0f00c9d281085d596c84c284423ccec6
chatbot8 <104543182+chatbot8@users.noreply.github.com>
2022-04-28 10:16:44 +0530

    Update covidbot.py

--- covidbot.py
+++ covidbot.py
@@ -3,7 +3,7 @@ import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) 
  
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters 
  
